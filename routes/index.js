b642fa523c9b927a16c6c5f7fe4fb515c50b9c4b
ofir h <houbara0@gmail.com>
2020-03-15 21:31:41 +0200

    second commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 import express from 'express';
+import path from 'path';
 import api from './api';
 
 const router = express.Router();
 
 router.use('/api',api);
 
+router.use('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
+});
 export default router;
