bb24ce4ff767689ce88ae89fcdbd06e723167979
ofir h <houbara0@gmail.com>
2020-03-16 17:54:16 +0200

    third 1.3

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const app = require("./server.js");
 
 // //created by Zvi
 
-// const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-// const ssl_options = {
-//     key: fs.readFileSync('./ssl/privkey.pem'),
-//     cert: fs.readFileSync('./ssl/fullchain.pem')
-// };
+const ssl_options = {
+    key: fs.readFileSync('./ssl/privkey.pem'),
+    cert: fs.readFileSync('./ssl/fullchain.pem')
+};
 
-// const server = https.createServer(ssl_options, app);
+const server = https.createServer(ssl_options, app);
 
-// server.listen(port, function () {
-//     console.info(`Listening on port ${port}`);
-// });
+server.listen(port, function () {
+    console.info(`Listening on port ${port}`);
+});
 
 
 module.exports = app;
