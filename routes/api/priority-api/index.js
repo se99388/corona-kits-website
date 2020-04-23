f29c3dc8a25089d1873c5ba5d06e04c5a6fbcfed
ofir h <houbara0@gmail.com>
2020-04-23 13:53:55 +0300

    change the date starting for IMRP10243X

--- routes/api/priority-api/index.js
+++ routes/api/priority-api/index.js
@@ -47,8 +47,8 @@ router.get('/in-stock/:partname', async (req, res, next) => {
 
 //Corona-stock-status notes
 router.get('/all-kits-in-stock-notes', async (req, res, next) => {
-    try { 
-        
+    try {
+
         const url = "PORDERS?$filter=SUPNAME eq '55921' or SUPNAME eq '57046' or SUPNAME eq '55222' or SUPNAME eq '55289' or SUPNAME eq '55697' or SUPNAME eq '55044' or SUPNAME eq '57271' or SUPNAME eq '55349' or SUPNAME eq '56110' and CLOSEDBOOL eq ''&$expand=PORDERITEMS_SUBFORM($filter=CLOSEDBOOL eq '' and (PARTNAME eq 'TTS238' or PARTNAME eq 'CAP250' or PARTNAME eq 'IM235905' or PARTNAME eq 'IMHSP9655' or PARTNAME eq 'IMTCS0803' or PARTNAME eq 'IM235903' or PARTNAME eq 'IM7443004U' or PARTNAME eq 'IMRP10243X' or PARTNAME eq 'IMSDP0096' or PARTNAME eq 'MBC0097' or PARTNAME eq 'IMHW4412' or PARTNAME eq 'IMR2141' or PARTNAME eq 'IMMVN40004' or PARTNAME eq 'IMMVN40006'); $select=KLINE,PARTNAME,PDES,CLOSEDBOOL;$expand=PORDERITEMSTEXT_SUBFORM)&$select=SUPNAME,CURDATE,STATDES,ORDNAME";
 
         const response = await getPriorityApi(url);
@@ -59,7 +59,7 @@ router.get('/all-kits-in-stock-notes', async (req, res, next) => {
         res.json({ error });
     }
 })
- //Corona-stock-status 
+//Corona-stock-status 
 router.get('/all-kits-in-stock', async (req, res, next) => {
     try {
         const url = "LOGPART?$filter=PARTNAME eq 'IMRP10243X' or PARTNAME eq 'IM235905' or PARTNAME eq 'IM235903' or PARTNAME eq 'IMSDP0096' or PARTNAME eq 'IM7443004U' or PARTNAME eq 'IMHSP9655' or PARTNAME eq 'IMTCS0803' or PARTNAME eq 'TTS238' or PARTNAME eq 'CAP250' or PARTNAME eq 'MBC0097' or PARTNAME eq 'IMHW4412' or PARTNAME eq 'IMR2141' or PARTNAME eq 'IMMVN40004' or PARTNAME eq 'IMMVN40006' &$select=PARTNAME,EPARTDES&$expand=LOGCOUNTERS_SUBFORM($select=BALANCE,PORDERS)";
@@ -75,11 +75,25 @@ router.get('/all-kits-in-stock', async (req, res, next) => {
 //Corona-kits-users + Summery
 router.get('/labs-list-supply/:partname', async (req, res, next) => {
     try {
+        const baseUrl =` and (DOCDES eq 'משלוחים ללקוח' or DOCDES eq 'החזרה מלקוח')&$select=PARTNAME,STATDES,DOCDES,CDES,TQUANT,CURDATE,CUSTNAME,Y_8871_5_ESHB,Y_4795_5_ESHB&$orderby=CURDATE desc` 
+        let url = '';
+        const startDate = '2020-03-12';
         const partname = JSON.parse(req.params.partname);
-        const startDate='2020-03-12'
+// 'IMRP10243X' has no date limitation and this is why he has different query
+        const IMRP10243X = partname.find((singlePartname, index) => {
+            if (singlePartname === 'IMRP10243X') {
+                partname.splice([index], 1);
+                return singlePartname
+            };
+        });
         const oDataText = `PARTNAME eq '${partname.join(`' or PARTNAME eq '`)}'`;
+        if (IMRP10243X) {
+            const oDataIMRP10243X = `(PARTNAME eq '${IMRP10243X}' and STATDES eq 'סופית')`
+            url = `TRANSORDER_DN?$filter=((${oDataText}) and STATDES eq 'סופית' and CURDATE ge ${startDate}) or ${oDataIMRP10243X}${baseUrl}`;
+        } else {
+            url = `TRANSORDER_DN?$filter=(${oDataText}) and STATDES eq 'סופית' and CURDATE ge ${startDate}${baseUrl}`;
+        }
 
-        const url = `TRANSORDER_DN?$filter=(${oDataText}) and STATDES eq 'סופית' and CURDATE ge ${startDate} and (DOCDES eq 'משלוחים ללקוח' or DOCDES eq 'החזרה מלקוח')&$select=PARTNAME,STATDES,DOCDES,CDES,TQUANT,CURDATE,CUSTNAME,Y_8871_5_ESHB,Y_4795_5_ESHB&$orderby=CURDATE desc`;
         const encodedURI = encodeURI(url);
         const response = await getPriorityApi(encodedURI);
         res.json(response.data.value)
@@ -93,9 +107,9 @@ router.get('/labs-list-supply/:partname', async (req, res, next) => {
 //Corona-kits-users + Summery
 router.post('/labs-list-supply-by-date', async (req, res, next) => {
     try {
-        const {startDate, endDate} = req.body.date;
-        const {partname} = req.body;
- 
+        const { startDate, endDate } = req.body.date;
+        const { partname } = req.body;
+
         const oDataText = `PARTNAME eq '${partname.join(`' or PARTNAME eq '`)}'`;
         const url = `TRANSORDER_DN?$filter=(${oDataText}) and STATDES eq 'סופית' and CURDATE ge ${startDate} and CURDATE le ${endDate} and (DOCDES eq 'משלוחים ללקוח' or DOCDES eq 'החזרה מלקוח')&$select=PARTNAME,STATDES,DOCDES,CDES,TQUANT,CURDATE,CUSTNAME,Y_8871_5_ESHB,Y_4795_5_ESHB&$orderby=CURDATE desc`;
         const encodedURI = encodeURI(url);
